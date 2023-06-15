ce565ff6a92b8e52ac20186b2676b2cbfbb5daa8
Kingsley Igbor <chidoziek2@gmail.com>
2023-06-15 15:55:54 +0100

    fixed linters error

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: {
-        index: path.resolve(__dirname, "./src/index.js"),
-    },
-    devServer: {
-        static: "./dist",
-    },
-    optimization: {
-        runtimeChunk: "single",
-    },
-    output: {
-        path: path.resolve(__dirname, "./dist"),
-        filename: "[name].bundle.js",
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Output Management",
-            template: path.resolve(__dirname, "./src/template.html"),
-        }),
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: path.resolve(__dirname, './src/template.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
+  },
 };
